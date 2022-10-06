71cb83de272d255328e2d499d1ebe5d35a2df232
Narukoshin <naru.koshin@outlook.jp>
2022-10-06 18:31:11 +0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -2,6 +2,7 @@
 
 updates_to_install=$(apt update 2>&1 | egrep -o "[0-9]+ package(s?) can be upgraded." | egrep -o "[0-9]+")
 update_log="/root/updater/logs/$(date +"%d-%m-%Y_%H-%M-update").log"
+main_dir="/root/updater"
 
 # checking for updates and installing them
 
@@ -10,6 +11,7 @@ if [[ updates_to_install -gt 0 ]]; then
         echo "$(apt list --upgradeable 2>&1)" >> $update_log
         export DEBIAN_FRONTEND=noninteractive
         apt upgrade -y >> $update_log 2>&1
+        apt autoremove -y > /dev/null # removing the files that aren't necessary anymore.
 fi
 
 # creating an archive for previous month
@@ -17,5 +19,5 @@ fi
 if (( $(date +"%-d") == 1 )); then
     prev_month=0`expr $(date +"%m") - 1`
     archive_name=$(date -d $(date +"%Y-$prev_month-%d") '+%B-%Y').tar.gz
-    tar -zcvf /root/updater/logs/$archive_name /root/updater/logs/*-$prev_month-$(date +"%Y")* --remove-files > /dev/null
+    tar -zcvf $main_dir/logs/$archive_name $main_dir/logs/*-$prev_month-$(date +"%Y")* --remove-files > /dev/null
 fi
